845001145960fae13c5d05a61545456f35bc3f76
spacexerq <bns2002@yandex.ru>
2023-06-29 06:34:49 +0300

    unbalanced problems release

--- transport_pr/class_data_block.py
+++ transport_pr/class_data_block.py
@@ -9,6 +9,8 @@ class TransportVS:
     """
 
     def __init__(self, a: np.array, b: np.array, c: np.array):
+        self.has_fict_column = None
+        self.has_fict_row = None
         self.a = a
         self.b = b
         self.c = c
@@ -59,3 +61,17 @@ class TransportVS:
         """Finds the best cell to recalculate potentials"""
         free_cells = tuple(zip(*np.nonzero(x == 0)))
         return free_cells[np.argmax([p['a'][i] + p['b'][j] - self.c[i][j] for i, j in free_cells])]
+
+    def add_supplier(self, volume) -> None:
+        e = np.ones(self.n)
+        self.c = np.row_stack((self.c, e))
+        self.a = np.append(self.a, volume)
+        if np.all(e == 0):
+            self.has_fict_row = True
+
+    def add_customer(self, volume) -> None:
+        e = np.ones(self.m)
+        self.c = np.column_stack((self.c, e))
+        self.b = np.append(self.b, volume)
+        if np.all(e == 0):
+            self.has_fict_column = True
